c040942e51478058a001d4094f7862f51936dcb8
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2023-01-08 17:06:52 +0200

    modified.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+
+const fs = require("fs");
+const app = express();
+
+app.get("/", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    const jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
+});
+app.listen(process.env.PORT || 3000);
